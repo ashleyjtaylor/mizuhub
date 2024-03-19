5824a971c30296f045cb636c0d51b2e48b49e20d
ashley taylor <ashleytaylor14@gmail.com>
2024-03-19 11:42:04 +0000

    chore: add http-status-codes

--- apps/api/src/server.ts
+++ apps/api/src/server.ts
@@ -1,5 +1,6 @@
 import express, { ErrorRequestHandler, NextFunction, Request, Response } from 'express'
 import bodyParser from 'body-parser'
+import { StatusCodes, ReasonPhrases } from 'http-status-codes'
 
 import logger from './logger/logger'
 
@@ -8,14 +9,22 @@ const app = express()
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: false }))
 
+app.get('/', (_req: Request, res: Response, _next: NextFunction) => {
+  return res.status(StatusCodes.OK).send(ReasonPhrases.OK)
+})
+
 app.get('/health', (_req: Request, res: Response, _next: NextFunction) => {
-  return res.status(200).send('ok')
+  return res.status(StatusCodes.OK).send(ReasonPhrases.OK)
+})
+
+app.get('*', (_req: Request, res: Response, _next: NextFunction) => {
+  return res.status(StatusCodes.NOT_FOUND).send(ReasonPhrases.NOT_FOUND)
 })
 
 app.use((error: ErrorRequestHandler, _req: Request, res: Response, _next: NextFunction) => {
   logger.error(error, error.name)
 
-  return res.status(500).json({ error: 'Something went wrong' })
+  return res.status(StatusCodes.INTERNAL_SERVER_ERROR).send(ReasonPhrases.INTERNAL_SERVER_ERROR)
 })
 
 export default app
