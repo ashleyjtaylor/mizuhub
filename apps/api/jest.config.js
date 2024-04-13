1414f1f7a27b8f3741fe62cb85d94611e07fa365
Ash <ashleyjtaylor@users.noreply.github.com>
2024-04-13 21:27:51 +0100

    chore: sonar monorepo setup (#6)
    * chore: sonar monorepo setup
    
    * chore: add sonar step to test
    
    * chore: update sonar-project.properties
    
    * chore: sonar debug
    
    * chore: sonar debug
    
    * chore: sonar debug
    
    * chore: debug coverage
    
    * chore: debug coverage
    
    * chore: debug coverage
    
    * chore: debug coverage

---
+++ apps/api/jest.config.js
@@ -0,0 +1,15 @@
+/** @type {import('ts-jest').JestConfigWithTsJest} */
+
+const rootConfig = require('../../jest.config')
+
+module.exports = {
+  ...rootConfig,
+  collectCoverageFrom: [
+    'src/**/*.ts',
+    '!src/index.ts',
+    '!src/database/**',
+    '!src/utils/logger.ts'
+  ],
+  setupFiles: ['<rootDir>/jest.setup.js'],
+  setupFilesAfterEnv: ['<rootDir>/jest.env.js']
+}
